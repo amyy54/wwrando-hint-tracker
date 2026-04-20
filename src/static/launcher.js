9c9b0c9514e37a4872ab09c6f5ef4abb08ac1398
Amy C <git@amyy.me>
2026-04-20 19:36:15 -0400

    Fix tracker routing issue

--- src/static/launcher.js
+++ src/static/launcher.js
@@ -1,6 +1,6 @@
 document.getElementById("launcher").addEventListener("click", (e) => {
   window.open(
-    "/tracker.html",
+    "tracker.html",
     "_blank",
     "width=1507,height=420,titlebar=0,menubar=0,toolbar=0",
   );
