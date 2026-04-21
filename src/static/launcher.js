8db64b54f3cb2a202720a00872c9955432da87ef
Amy C <git@amyy.me>
2026-04-20 23:13:21 -0400

    Adjust default launch size to 450

--- src/static/launcher.js
+++ src/static/launcher.js
@@ -2,6 +2,6 @@ document.getElementById("launcher").addEventListener("click", (e) => {
   window.open(
     "tracker.html",
     "_blank",
-    "width=1507,height=420,titlebar=0,menubar=0,toolbar=0",
+    "width=1507,height=450,titlebar=0,menubar=0,toolbar=0",
   );
 });
